59cec472ce00a30696d01512cb4ccd65058bd066
Giovanni Cardamone <g.cardamone2@gmail.com>
2020-08-14 04:22:24 +0200

    publishing

---
+++ docs/precache-manifest.6381e7c6f749cd8778310950380c484c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5c64659dc71f5848dd8c22c42dd54a",
+    "url": "https://giovannicardamone.github.io/curriculum/index.html"
+  },
+  {
+    "revision": "ea9c678afae7a7dadeb3",
+    "url": "https://giovannicardamone.github.io/curriculum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "882d4e721a8f17dd0a35",
+    "url": "https://giovannicardamone.github.io/curriculum/static/js/2.4253362a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://giovannicardamone.github.io/curriculum/static/js/2.4253362a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9c678afae7a7dadeb3",
+    "url": "https://giovannicardamone.github.io/curriculum/static/js/main.cf1c2f93.chunk.js"
+  },
+  {
+    "revision": "3c6b4d3e35a33dc549d0",
+    "url": "https://giovannicardamone.github.io/curriculum/static/js/runtime-main.c280a9bb.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9b8106803904cc930300f4b57967608.js"
+  "https://giovannicardamone.github.io/curriculum/precache-manifest.6381e7c6f749cd8778310950380c484c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://giovannicardamone.github.io/curriculum/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
