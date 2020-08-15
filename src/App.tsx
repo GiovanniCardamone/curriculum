079d47462178de113e1543bd57a3695e23b1b01f
Giovanni Cardamone <g.cardamone2@gmail.com>
2020-08-15 20:38:55 +0200

    added skillbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 import Papers from './components/Papers'
-import PageA4 from './components/PageA4'
 import PrintButton from './components/PrintButton'
 
 import FirstPage from './pages/FirstPage'
@@ -13,9 +11,8 @@ import ThirdPage from './pages/ThirdPage'
 function App() {
   return (
     <div className='App'>
-      current in development! Hang on :)
-      {/* <Papers papers={[<FirstPage />, <SecondPage />, <ThirdPage />]} />
-      <PrintButton onClick={() => {}} /> */}
+      <Papers papers={[<FirstPage />, <SecondPage />, <ThirdPage />]} />
+      <PrintButton onClick={() => {}} />
     </div>
   )
 }
--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -15,28 +15,28 @@ export default function FirstPage({}: FirstPageProp) {
           <span className='name'>Giovanni Cardamone</span>
         </div>
         <div className='skillprogress-container'>
-          <SkillProgress language='C' progress={85} />
-          <SkillProgress language='C++' progress={85} />
-          <SkillProgress language='C#' progress={85} />
-          <SkillProgress language='Java' progress={85} />
-          <SkillProgress language='Python' progress={85} />
-          <SkillProgress language='Php' progress={85} />
-          <SkillProgress language='Kotlin' progress={85} />
+          <SkillProgress language='C' progress={15} />
+          <SkillProgress language='C++' progress={25} />
+          <SkillProgress language='C#' progress={35} />
+          <SkillProgress language='Java' progress={45} />
+          <SkillProgress language='Python' progress={55} />
+          <SkillProgress language='Php' progress={65} />
+          <SkillProgress language='Kotlin' progress={75} />
           <SkillProgress language='Dart' progress={85} />
-          <SkillProgress language='Lua' progress={85} />
-          <SkillProgress language='Javascript' progress={85} />
-          <SkillProgress language='Typescript' progress={85} />
-          <SkillProgress language='Bash/Sh' progress={85} />
-          <SkillProgress language='HTML & CSS' progress={85} />
+          <SkillProgress language='Lua' progress={95} />
+          <SkillProgress language='Javascript' progress={100} />
+          <SkillProgress language='Typescript' progress={5} />
+          <SkillProgress language='Bash/Sh' progress={50} />
+          <SkillProgress language='HTML & CSS' progress={25} />
         </div>
 
-        <div className='contact-container'>
+        {/* <div className='contact-container'>
           <Contact type='email' contact='g.cardamone2@gmail.com' />
-        </div>
+        </div> */}
       </div>
 
       <div className='firstpage--rightside'>
-        <span className='name'></span>
+        <span className='section-title'>Bio</span>
       </div>
     </div>
   )
@@ -48,16 +48,34 @@ interface SkillProgressProp {
 }
 
 function SkillProgress({ language, progress }: SkillProgressProp) {
+  const min = 'ef0078'
+  const max = '90ee02'
+
+  const mix = (start: number, end: number, percent: number) => start + percent * (end - start)
+
+  const progressColor = () => {
+    const r1 = parseInt(min.substr(0, 2), 16)
+    const g1 = parseInt(min.substr(2, 2), 16)
+    const b1 = parseInt(min.substr(4, 2), 16)
+
+    const r2 = parseInt(max.substr(0, 2), 16)
+    const g2 = parseInt(max.substr(2, 2), 16)
+    const b2 = parseInt(max.substr(4, 2), 16)
+
+    const q = progress / 100
+    return `rgb(${Math.round(mix(r1, r2, q))}, ${Math.round(mix(g1, g2, q))}, ${Math.round(mix(b1, b2, q))})`
+  }
+
   return (
     <div className='progress'>
-      <div className='center-text language'>
+      <div className='center-text language static-size-1'>
         <span className='clear-text'>{language}</span>
       </div>
-      <div className='center-text percentage'>
+      <div className='center-text percentage static-size-2'>
         <span className='clear-text'>{progress}%</span>
       </div>
 
-      <div className='progress-arrow' style={{ width: progress + '%' }}></div>
+      <div className='progress-arrow' style={{ width: progress + '%', backgroundColor: progressColor() }}></div>
       <div className='arrow-head' style={{ width: progress + '%' }}></div>
     </div>
   )
